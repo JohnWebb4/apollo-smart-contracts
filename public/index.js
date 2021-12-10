2453b62fa37a9b606e50bb65d8dea0eb6e78b86a
John Webb <john.webb94@gmail.com>
2021-12-10 04:25:10 -0700

    Getting the auth signature

--- public/index.js
+++ public/index.js
@@ -1,1 +1,32 @@
-console.log("hi");
+const message = "";
+
+async function main() {
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    try {
+      // ask user permission to access his accounts
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+
+      const accounts = await web3.eth.getAccounts();
+
+      const hashMessage = web3.eth.accounts.hashMessage("I'd like to sign in");
+
+      const signature = await web3.eth.sign(hashMessage, accounts[0]);
+
+      const signatureRef = document.createElement("p");
+      signatureRef.textContent = signature;
+
+      document.body.appendChild(signatureRef);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    throw "Must install MetaMask";
+  }
+}
+
+function getEIPString(message) {
+  return "\x19Ethereum Signed Message:\n" + message.length + message;
+}
+
+window.addEventListener("load", main);
